30c642bc1dbc920a88fd7e407b5949ba1c75f7f7
EstefaSora125 <estefayiso@gmail.com>
2022-10-30 10:26:14 -0500

    delete uppercase

--- src/routes/routesAdmin.js
+++ src/routes/routesAdmin.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const {createDependencie} = require("../controller/admin/controllerDependecies")
 const {createPatient,getPatient,getPatiensOrderAZ, getPatiensOrderZA,getSpecificPatient, getNamePatient, updatePatient} = require("../controller/admin/controllerPatients")
-const {createProductTracings, getItemTracingProduct, getPersonProductTracing, getPacientProductTracing} = require ('../controller/admin/controllerProductTracings')
+const {createProductTracings, getItemPT, getPersonPT, getPacientPT, getItemPacientPT, getItemPersonPT, getItemDestinyPT,getPacientDestinyPT,
+        getPersonPacientPT, getPersonDestinyPT} = require ('../controller/admin/controllerProductTracings')
 
 //---------------------Dependencies--------------------------------
 router.post('/createDependecie', createDependencie)
@@ -20,9 +21,16 @@ router.put('/updatePatient', updatePatient)
 // --------------------------- Product Tracings ----------------------
 
 router.post('/createProductTracing', createProductTracings)
-router.post('/getItemProductsTracing', getItemTracingProduct)
-router.post('/getPersonProductsTracing', getPersonProductTracing)
-router.post('/getPacientProductsTracing', getPacientProductTracing)
+router.post('/getItemProductsTracing', getItemPT)
+router.post('/getPersonProductsTracing', getPersonPT)
+router.post('/getPacientProductsTracing', getPacientPT)
+router.post('/getItemPersonProductsTracing', getItemPersonPT)
+router.post('/getItemPacientProductsTracing', getItemPacientPT)
+router.post('/getItemDestinyProductsTracing', getItemDestinyPT)
+router.post('/getPersonPacientProductsTracing', getPersonPacientPT)
+router.post('/getPersonDestinyProductsTracing', getPersonDestinyPT)
+router.post('/getPacientDestinyProductsTracing', getPacientDestinyPT)
+
 
 //-------------------------Item---------------------------------
 
