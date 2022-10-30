8d84799498d8f371bf842aae292afbd328df3ebb
RaulV8 <raul.valencia01@uptc.edu.co>
2022-10-29 22:58:34 -0500

    Create getPersonAll and change endpoints persons

--- src/routes/routesAdminUsers.js
+++ src/routes/routesAdminUsers.js
@@ -1,16 +1,15 @@
 const router = require('express').Router()
 
-const {createPersons,getPersons, getIdPersons,getRolPersons,updatePersons,deletePersons, getPersonsOrderAZ, getPersonsOrderZA} = require("../controller/admin/persons/controllerPerson")
+const {createPersons, createPersonAll,getPersons, getIdPersons,updatePersons,deletePersons} = require("../controller/admin/persons/controllerPerson")
 const {createRole,getRoles,getIdRoles, updateRol, deleteRoles} = require("../controller/admin/persons/controllerRoles")
 const {createUserRoles, getUserRoles, getIdUserRoles, updateUserRoles, deleteUserRoles} = require ("../controller/admin/persons/controllerUserRoles")
 
 //---------------------- Users -----------------------------------
 
 router.post('/createUser', createPersons)
-router.get('/persons',getPersons)
-router.get('/personsAZ',getPersonsOrderAZ)
-router.get('/personsZA',getPersonsOrderZA)
-router.get('/personsId',getIdPersons)
+router.post('/createPersonAll', createPersonAll)
+router.post('/persons',getPersons)
+router.post('/personsId',getIdPersons)
 router.put('/updatePerson',updatePersons)
 router.patch('/deletePerson', deletePersons)
 
