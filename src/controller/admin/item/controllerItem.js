793383b3755dbe5beb916602937416a8e809b82a
RaulV8 <raul.valencia01@uptc.edu.co>
2022-11-04 03:09:02 -0500

    add modules create and assing item

--- src/controller/admin/item/controllerItem.js
+++ src/controller/admin/item/controllerItem.js
@@ -1,3 +1,73 @@
 const {PrismaClient} = require("@prisma/client")
 const prisma = new PrismaClient()
 
+const createItem = async (req, res) =>{
+    try {
+        const product = await prisma.products.create({
+            data:{
+                ID_PRODUCT: req.body.id_product,
+                PRODUCT_NAME: req.body.product_name.charAt(0).toUpperCase() +req.body.product_name.slice(1),
+                MEASUREMENT_UNITS: req.body.measurement_units,
+                TYPE_PRODUCT: req.body.type_product,
+            }
+        })
+
+        const feature = await prisma.feature_products.create({
+            data:{
+                EXPIRATION_DATE : new Date (req.body.expiration_date),
+                QUANTITY_PER_UNIT: req.body.quantity_per_unit,
+                PRICE_PER_UNIT: req.body.price_per_unit,
+                INVIMA: req.body.invima,
+                MANUFACTURING_DATE: new Date (req.body.manufacturing_date)
+            }
+        })
+
+        await prisma.item.create({
+            data:{
+                PRESENTATION: req.body.presentation,
+                QUANTITY: req.body.quantity,
+                ID_PRODUCT: product.ID_PRODUCT,
+                ID_FEATURE: feature.ID_FEATURE,
+                ID_DEPENDENCIE: req.body.id_dependencie
+            }
+        })
+        res.send({
+            message: "Item creado con éxito"
+        });
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento de crear el item"
+        });
+        console.log(error)
+    }
+}
+
+const assingItem = async (req, res) =>{
+    try {
+
+        await prisma.item.create({
+            data:{
+                PRESENTATION: req.body.presentation,
+                QUANTITY: req.body.quantity,
+                ID_PRODUCT: req.body.id_product,
+                ID_FEATURE: req.body.id_feature,
+                ID_DEPENDENCIE: req.body.id_dependencie
+            }
+        })
+        res.send({
+            message: "Item asignado con éxito"
+        });
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento de asignar un item"
+        });
+        console.log(error)
+    }
+}
+
+
+
+module.exports = {
+    createItem,
+    assingItem
+}
--- src/routes/admin/routesAdminItem.js
+++ src/routes/admin/routesAdminItem.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const {getBrand, createBrand, updateBrand} = require('../../controller/admin/item/controllerBrands')
 const {getFeatureProducts, updateFeatureProduct, createFeatureProducts} = require('../../controller/admin/item/controllerFeatureProduct')
 const {getProduct, getItemProductDepartment,getSpecificProduct,createProducts, updateProduct, getItemProduct} = require('../../controller/admin/item/controllerProduct')
-
+const {createItem, assingItem} = require("../../controller/admin/item/controllerItem")
 
 //---------------------- Brands -----------------------------------
 
@@ -25,5 +25,10 @@ router.get('/getItemProductDepartment',getItemProductDepartment)
 router.post('/getSpecificProduct',getSpecificProduct)
 router.put('/updateProduct',updateProduct)
 router.get('/getItemProduct', getItemProduct)
+
+//-------------------------------- Item -------------------------------
+
+router.post('/createItem',createItem)
+router.post('/assingItem',assingItem)
 
 module.exports = router
