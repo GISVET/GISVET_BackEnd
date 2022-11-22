af0191a1b14980fa7fd74569f71a1f5ebc49e3c7
RaulV8 <86920089+RaulV8@users.noreply.github.com>
2022-11-22 00:56:26 -0500

    Update Bodega

--- src/routes/users/routesBodega.js
+++ src/routes/users/routesBodega.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const {getProductBodega, createItem,createBrand} = require('../../controller/users/controllerUserBodega')
+const {getProductBodega, createItem,createBrand,assingItem} = require('../../controller/users/controllerUserBodega')
 
 router.put('/createItem',createItem)
 router.post('/getProduct',getProductBodega)
 router.post('/createBrand',createBrand)
+router.post('/assingItem',assingItem)
 
 module.exports = router
