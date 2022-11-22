62550b1f1539f946f26c6ea3af03c8e6ac9b60fb
RaulV8 <86920089+RaulV8@users.noreply.github.com>
2022-11-22 00:56:51 -0500

    Update Bodega

--- src/controller/users/controllerUserBodega.js
+++ src/controller/users/controllerUserBodega.js
@@ -171,8 +171,76 @@ const createBrand = async (req, res) =>{
     }
 }
 
+const assingItem = async (req, res) =>{
+    try {
+        const idBrand =  req.body.id_brand
+        const nameBrand = req.body.name_brand
+        const product = req.body.id_product
+
+        const data = await prisma.brands.findUnique({
+            where:{
+                ID_BRAND: idBrand
+            }
+        })
+
+        if (data === null){
+            await prisma.brands.create({
+                data:{
+                    NAME_BRAND: nameBrand
+                }
+            })
+            await prisma.product_brand.create({
+                data:{
+                    ID_BRAND: idBrand,
+                    ID_PRODUCT: product,
+                }       
+            })
+
+        }else{
+            await prisma.product_brand.create({
+                data:{
+                    ID_BRAND: idBrand,
+                    ID_PRODUCT: product,
+                }       
+            })
+        }
+
+        const feature = await prisma.feature_products.create({
+            data:{
+                EXPIRATION_DATE : new Date (req.body.expiration_date),
+                QUANTITY_PER_UNIT: req.body.quantity_per_unit,
+                PRICE_PER_UNIT: req.body.price_per_unit,
+                INVIMA: req.body.invima,
+                MANUFACTURING_DATE: new Date (req.body.manufacturing_date),
+                ID_BOX: req.body.id_box,
+                STATE: "AC"                           
+            }
+        })
+
+        await prisma.item.create({
+            data:{
+                PRESENTATION: req.body.presentation,
+                QUANTITY: req.body.quantity,
+                ID_PRODUCT: product,
+                ID_FEATURE: feature.ID_FEATURE,
+                ID_DEPENDENCIE: req.body.id_dependencie
+            }
+        })
+        res.send({
+            message: "Item asignado con éxito"
+        });
+    } catch (error) {
+        res.send({
+            message: "Ocurrió un error al momento de asignar un item"
+        });
+        console.log(error)
+    }
+}
+
+
 module.exports = {
     getProductBodega,
     createItem,
-    createBrand
+    createBrand,
+    assingItem
 }
