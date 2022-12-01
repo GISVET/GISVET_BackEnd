befe47034a8ca8a6c359d7b03887d2768c5125df
ThisName <48261853+EstefaSora125@users.noreply.github.com>
2022-12-01 10:19:09 -0500

    Create item

--- src/controller/admin/item/controllerItem.js
+++ src/controller/admin/item/controllerItem.js
@@ -3,38 +3,47 @@ const prisma = new PrismaClient()
 
 const createItem = async (req, res) =>{
     try {
-        const product = await prisma.products.create({
-            data:{
-                PRODUCT_NAME: req.body.product_name.charAt(0).toUpperCase() +req.body.product_name.slice(1),
-                MEASUREMENT_UNITS: req.body.measurement_units,
-                TYPE_PRODUCT: req.body.type_product
+        const nameBrand = req.body.name_brand
+        const productName = req.body.product_name.charAt(0).toUpperCase() + req.body.product_name.slice(1)
+        
+        const searchProduct = await prisma.products.findMany({
+            where: {
+                PRODUCT_NAME: productName
             }
         })
 
-        const feature = await prisma.feature_products.create({
-            data:{
-                EXPIRATION_DATE : new Date (req.body.expiration_date),
-                QUANTITY_PER_UNIT: req.body.quantity_per_unit,
-                PRICE_PER_UNIT: req.body.price_per_unit,
-                INVIMA: req.body.invima,
-                MANUFACTURING_DATE: new Date (req.body.manufacturing_date),
-                ID_BOX: req.body.id_box,
-                STATE: "AC"
+        const searchBrand = await prisma.brands.findUnique({
+            where:{
+                NAME_BRAND: nameBrand
             }
         })
 
-        await prisma.item.create({
-            data:{
-                PRESENTATION: req.body.presentation,
-                QUANTITY: req.body.quantity,
-                ID_PRODUCT: product.ID_PRODUCT,
-                ID_FEATURE: feature.ID_FEATURE,
-                ID_DEPENDENCIE: req.body.id_dependencie
-            }
-        })
-        res.send({
-            message: "Item creado con éxito"
-        });
+        if(searchProduct[0] === undefined){
+            if(searchBrand != null){
+
+                addProduct(res, req, searchBrand)
+                res.send({
+                    message: "Item creado con éxito"
+                });            
+            }else{
+                const brand = await prisma.brands.create({
+                    data:{
+                        NAME_BRAND: nameBrand
+                    }
+                })           
+
+                addProduct(res, req, searchBrand)
+                res.send({
+                    message: "Item creado con éxito"
+                });
+            }        
+            
+        }else{
+            res.status(400).send({
+                message: "El producto ingresado ya existe"
+            }) 
+        }  
+        
     } catch (error) {
         res.send({
             message: "Ocurrió un error al momento de crear el item"
@@ -108,6 +117,8 @@ const assingItem = async (req, res) =>{
         console.log(error)
     }
 }
+
+
 
 
 
