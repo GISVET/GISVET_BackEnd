fe6e55ed41fd7c773606c4e0e5ab6291293c0648
RaulV8 <86920089+RaulV8@users.noreply.github.com>
2022-12-01 11:15:38 -0500

    Update route farmacia

--- src/routes/users/routesFarmcia.js
+++ src/routes/users/routesFarmcia.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
 const {sendProducts} = require('../../controller/users/controllerUserFarmacia')
 const {generateToken, getProducts} = require('../../controller/users/general')
+const {createPatient,getPatient,getPatiensOrderAZ, getPatiensOrderZA,getSpecificPatient, getNamePatient, updatePatient} = require("../../controller/admin/controllerPatients")
 
 router.post('/getProducts',getProducts)
 router.post('/sendProducts',sendProducts)
 router.post('/generateToken', generateToken)
+
+//------------------------Patients -----------------------------------
+
+router.post('/createPatient', createPatient)
+router.get('/getPatient', getPatient)
+router.get('/getPatientsOrderAZ',getPatiensOrderAZ)
+router.get('/getPatientsOrderZA',getPatiensOrderZA)
+router.post('/getSpecificPatient', getSpecificPatient)
+router.post('/getNamePatient', getNamePatient)
+router.put('/updatePatient', updatePatient)
 
 module.exports = router
