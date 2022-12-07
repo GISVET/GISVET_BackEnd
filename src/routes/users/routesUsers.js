fd0f7cd5ed52e52058f3d85e71acfeb44930b5f7
RaulV8 <86920089+RaulV8@users.noreply.github.com>
2022-12-07 15:55:53 -0500

    Add changes

--- src/routes/users/routesUsers.js
+++ src/routes/users/routesUsers.js
@@ -1,10 +1,14 @@
 const router = require('express').Router()
 const {getDependeciePersons} =  require('../../controller/admin/persons/controllerPerson')
+const {getPatient} = require("../../controller/admin/controllerPatients")
+const {getProductTraicing} =  require("../../controller/admin/controllerProductTracings")
 
 router.use("/Bodega", require('./routesBodega'))
 router.use("/Farmacia", require('./routesFarmcia'))
 router.use("/Consultorio", require('./routesConsultorio'))
 
 router.post('/getDependeciePersons', getDependeciePersons)
+router.get('/getPatient', getPatient)
+router.post('/getProductTraicing', getProductTraicing)
 
 module.exports = router
