aebc1f1be2812d260beaddef304abb617c111563
RaulV8 <86920089+RaulV8@users.noreply.github.com>
2022-12-11 15:02:31 -0500

    New datas Docs

--- src/routes/admin/routesAdmin.js
+++ src/routes/admin/routesAdmin.js
@@ -57,12 +57,102 @@ router.post('/createDependecie', createDependencie)
  *              order_name: asc
  */
 router.post('/getDependencies', getDependencies)
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Get id dependencies:
+ *          type: object
+ *          description: El endpoint /Admin/getIdDependencies permite obtener una dependecia en especifico por medio del id
+ *          properties:
+ *              id_dependencie:
+ *                  type: integer
+ *                  description: Id de la dependecia que se desea obtner
+ *          require:
+ *               - id_dependencie
+ *          example:
+ *              id_dependencie: 1
+ */
 router.post('/getIdDependencies', getIdDependencies)
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Assing dependencie to person:
+ *          type: object
+ *          description: El endpoint /Admin/createDependencieUser permite asignarle una dependecia a un usuario
+ *          properties:
+ *              id_person:
+ *                  type: integer
+ *                  description: Id de la persona que se le asignará la dependecia
+ *              id_dependencie:
+ *                  type: integer
+ *                  description: Id de la dependecia que se le asignará a el usuario
+ *          require:
+ *               - id_person
+ *               - id_dependencie
+ *          example:
+ *              id_person: 1
+ *              id_dependencie: 1
+ */
 router.post('/createDependencieUser', createDependencieUser)
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Actualizara dependencia:
+ *          type: object
+ *          description: El endpoint /Admin/updateDependecie permite actulizara una dependecia
+ *          properties:
+ *              id_dependencie:
+ *                  type: integer
+ *                  description: Id de la dependecia a actulizar
+ *              dependencie_name:
+ *                  type: string
+ *                  description: Nuevo nombre de la dependecia
+ *              type_dependencie: 
+ *                  type: string
+ *                  description: Nuevo tipo de dependecia
+ *          require:
+ *               - id_dependencie
+ *               - dependencie_name
+ *               - type_dependencie
+ *          example:
+ *              id_dependencie: 1
+ *              dependencie_name: nueva dependecia
+ *              type_dependencie: B
+ */
 router.put('/updateDependecie', updateDependecie)
 
 //------------------------Patients -----------------------------------
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Crear paciente:
+ *          type: object
+ *          description: El endpoint /Admin/updateDependecie permite actulizara una dependecia
+ *          properties:
+ *              id_dependencie:
+ *                  type: integer
+ *                  description: Id de la dependecia a actulizar
+ *              dependencie_name:
+ *                  type: string
+ *                  description: Nuevo nombre de la dependecia
+ *              type_dependencie: 
+ *                  type: string
+ *                  description: Nuevo tipo de dependecia
+ *          require:
+ *               - id_dependencie
+ *               - dependencie_name
+ *               - type_dependencie
+ *          example:
+ *              id_dependencie: 1
+ *              dependencie_name: nueva dependecia
+ *              type_dependencie: B
+ */
 router.post('/createPatient', createPatient)
 router.get('/getPatient', getPatient)
 router.get('/getPatientsOrderAZ',getPatiensOrderAZ)
